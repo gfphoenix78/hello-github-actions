dbaa0515354fd81d0069ab746791dfd7e8546b7c
Dark Sword <gfphoenix78@gmail.com>
2021-05-29 20:27:31 +0800

    build wg

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/sh -l
 
+echo "################################################"
+echo "CWD = '`pwd`'"
+echo "################################################"
+
 pacman -Sy
-pacman -S --noconfirm git make
+pacman -S --noconfirm git make mingw-w64-gcc imagemagick librsvg
 
 pacman -Q | grep libc
 
 mkdir ~/workspace
 cd ~/workspace
 git clone https://github.com/wireguard/wireguard-windows
-#cd wireguard-windows
+cd wireguard-windows
+
+make amd64/wireguard.exe x86/wireguard.exe
 
 echo "ALL IS WELL"
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
+echo "Hello world my name is $INPUT_MY_NAME"
