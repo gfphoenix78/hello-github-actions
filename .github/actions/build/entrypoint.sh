ded5842563f99007e7f597a54f9f56c644236aac
Dark Sword <gfphoenix78@gmail.com>
2021-05-29 20:53:51 +0800

    test 1

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -3,18 +3,24 @@
 echo "################################################"
 echo "CWD = '`pwd`'"
 echo "################################################"
+ls
 
 pacman -Sy
 pacman -S --noconfirm git make mingw-w64-gcc imagemagick librsvg
 
 pacman -Q | grep libc
 
-mkdir ~/workspace
-cd ~/workspace
+#mkdir ~/workspace
+#cd ~/workspace
 git clone https://github.com/wireguard/wireguard-windows
+
+pushd
 cd wireguard-windows
-
 make amd64/wireguard.exe x86/wireguard.exe
+popd
 
+cp wireguard-windows/amd64/wireguard.exe wg_64
 echo "ALL IS WELL"
 echo "Hello world my name is $INPUT_MY_NAME"
+ls 
+ls -l wg*
